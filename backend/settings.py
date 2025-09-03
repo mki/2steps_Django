701faac47d2c1dda0e9e7f8d0582714d453be22d
Costa <kosschoo@gmail.com>
2025-09-03 11:10:26 +0400

    Read ALLOWED_HOSTS from env

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-guvlb#3e&ly6f#i3xymo7sc_1jd%(fg&n_rw0g@xn2^4b^oc_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
